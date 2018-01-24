90071419588fce4f77353aa9dacb447ec474ba51
jordan3900 <m.iordanize@gmail.com>
2018-01-24 13:22:55 +0200

    Add Exercise

---
+++ Exercise-StackAndQueue/08.RecursiveFibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.RecursiveFibonacci
+{
+    class RecursiveFibonacci
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+              Fibonacci(0, 1, 1, n);
+            
+        }
+
+        private static void Fibonacci(long a, long b, long counter, long n)
+        {
+            
+            if (counter<=n)
+            {
+                Fibonacci(b, a + b, counter + 1, n);
+            }
+            else
+            {
+                Console.WriteLine(a);
+            }
+
+            
+            
+        }
+    }
+}
