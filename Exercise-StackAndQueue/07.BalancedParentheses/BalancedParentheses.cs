1b1653dfd64769ac1118083aa5a1d97d74473ec6
jordan3900 <m.iordanize@gmail.com>
2018-01-24 12:42:22 +0200

    Add Exercise

---
+++ Exercise-StackAndQueue/07.BalancedParentheses/BalancedParentheses.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.BalancedParentheses
+{
+    class BalancedParentheses
+    {
+        static void Main(string[] args)
+        {
+            char[] parentheses = Console.ReadLine().ToCharArray();
+            Stack<char> openedParentheses = new Stack<char>();
+            char[] openingParentheses = new char[] { '(', '[', '{' };
+
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                if (openingParentheses.Contains(parentheses[i]))
+                {
+                    openedParentheses.Push(parentheses[i]);
+                }
+                else
+                {
+                    if (openedParentheses.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    switch (parentheses[i])
+                    {
+                        case ']':
+                            if (openedParentheses.Pop() != '[')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+                        case '}':
+                            if (openedParentheses.Pop() != '{')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+                        case ')':
+                            if (openedParentheses.Pop() != '(')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+               
+            }
+            if (openedParentheses.Count==0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
