e74a7fa10af7f54a897cd6fed77dafbb4201324b
jordan3900 <m.iordanize@gmail.com>
2018-01-24 22:38:19 +0200

    Add exercises

---
+++ Exercise-StackAndQueue/09.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.StackFibonacci
+{
+    class StackFibonacci
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Stack<long> fibonacci = new Stack<long>();
+
+            
+            fibonacci.Push(1);
+            fibonacci.Push(1);
+            int a = 1;
+            for (int i = 2; i < n; i++)
+            {
+                long fibPrev = fibonacci.Pop();
+                long fibNext = fibonacci.Peek() + fibPrev;
+
+                fibonacci.Push(fibPrev);
+                fibonacci.Push(fibNext);
+            }
+            Console.WriteLine(fibonacci.Pop());
+        }
+    }
+}
---
+++ Exercise-StackAndQueue/10.SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.SimpleTextEditor
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder builder = new StringBuilder();
+            Stack<string> textHoleder = new Stack<string>();
+
+
+            Console.WriteLine(builder);
+            for (int i = 0; i < n; i++)
+            {
+                string[] operations = Console.ReadLine().Split();
+                int operation = int.Parse(operations[0]);
+                switch (operation)
+                {
+                    case 1:
+                        builder.Append(operations[1]);
+                            textHoleder.Push(builder.ToString());
+                        break;
+                    case 2:
+                        builder.Remove(0, int.Parse(operations[1]));
+                            textHoleder.Push(builder.ToString());
+                        break;
+                    case 3:
+                        Console.WriteLine(builder[int.Parse(operations[1]) - 1]); ;
+                        break;
+                    case 4:
+                        builder.Clear();
+                        textHoleder.Pop();
+                        builder.Append(textHoleder.Peek());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
