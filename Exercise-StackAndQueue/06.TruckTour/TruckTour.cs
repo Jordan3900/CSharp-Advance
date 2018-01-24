8db9de01c840f4fc25ca8bddf04fa878c44b6e40
jordan3900 <m.iordanize@gmail.com>
2018-01-24 12:17:42 +0200

    Add TruckTour exercice

---
+++ Exercise-StackAndQueue/06.TruckTour/TruckTour.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.TruckTour
+{
+    class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var queue = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var pump = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                queue.Enqueue(pump);
+
+            }
+
+            for (int currentStart = 0; currentStart < queue.Count-1; currentStart++)
+            {
+                int fuel = 0;
+                bool isSolution = true;
+
+                for (int pumpPassed = 0; pumpPassed < n; pumpPassed++)
+                {
+                    var currentPump = queue.Dequeue();
+                    
+                    int pumpFuel = currentPump[0];
+                    int nextPumpDistance = currentPump[1];
+                    queue.Enqueue(currentPump);
+                    fuel += pumpFuel - nextPumpDistance;
+                    if (fuel < 0)
+                    {
+                        currentStart += pumpPassed;
+                        isSolution = false;
+                        break;
+                    }
+
+                }
+                if (isSolution)
+                {
+                    Console.WriteLine(currentStart);
+                    Environment.Exit(0);
+                }
+            }
+
+        }
+    }
+}
