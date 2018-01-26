b32779488257b24fdb7761455062505568acb874
jordan3900 <m.iordanize@gmail.com>
2018-01-26 11:37:58 +0200

    Add exercises

---
+++ MultiDimensionalAarrays/01.SumMatrixElements/SumMatrixElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace _01.SumMatrixElements
+{
+    class SumMatrixElements
+    {
+        static void Main(string[] args)
+        {
+            int[] rowAndCol = Console.ReadLine().Split(new char[] { ',' }).Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowAndCol[0], rowAndCol[1]];
+
+            for (int row = 0; row < rowAndCol[0]; row++)
+            {
+                int[] rowValues = Console.ReadLine().Split(new char[] { ','}).Select(int.Parse).ToArray();
+                for (int col = 0; col < rowAndCol[1]; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+            Console.WriteLine(rowAndCol[0]);
+            Console.WriteLine(rowAndCol[1]);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ MultiDimensionalAarrays/02.SquareWithMaximumSum/SquareWithMaximumSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+namespace _02.SquareWithMaximumSum
+{
+    class SquareWithMaximumSum
+    {
+        static void Main(string[] args)
+        {
+            int[] colsAndRows = Console.ReadLine().Split(new char[] { ',' }).Select(int.Parse).ToArray();
+            int[,] matrix = new int[colsAndRows[0], colsAndRows[1]];
+            int[] top = new int[2];
+            int[] bottom = new int[2];
+            int maxSum = int.MinValue;
+
+            for (int row = 0; row < colsAndRows[0]; row++)
+            {
+                int[] rowValues = Console.ReadLine().Split(new char[] { ',' }).Select(int.Parse).ToArray();
+                for (int col = 0; col < colsAndRows[1]; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row,col] + matrix[row,col+1] + matrix[row+1,col] + matrix[row + 1, col+1];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        top[0] = matrix[row, col];
+                        top[1] = matrix[row, col + 1];
+                        bottom[0] = matrix[row+1, col];
+                        bottom[1] = matrix[row+1, col + 1];
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ",top));
+            Console.WriteLine(string.Join(" ", bottom));
+            Console.WriteLine(maxSum);
+        }
+    }
+}
