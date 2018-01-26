4ff1ac11c28e07fb0e7b15f049e8796c11b415e0
jordan3900 <m.iordanize@gmail.com>
2018-01-26 15:03:46 +0200

    Add exercises

---
+++ MultiDimensionalAarrays/03.GroupNumbers/GroupNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GroupNumbers
+{
+    class GroupNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new char[] { ',' }).Select(int.Parse).ToArray();
+            int[] sizes = new int[3];
+
+            foreach (var number in numbers)
+            {
+                sizes[Math.Abs(number) % 3]++;
+            }
+            int[][] jaggedArray = new int[3][];
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                jaggedArray[i] = new int[sizes[i]];
+            }
+            int[] index = new int[3];
+            foreach (var number in numbers)
+            {
+                var reminder = Math.Abs(number) % 3;
+                jaggedArray[reminder][index[reminder]] = number;
+                index[reminder]++;
+            }
+            Console.WriteLine(string.Join(" ", jaggedArray[0]));
+            Console.WriteLine(string.Join(" ", jaggedArray[1]));
+            Console.WriteLine(string.Join(" ", jaggedArray[2]));
+        }
+    }
+}
---
+++ MultiDimensionalAarrays/04.PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.PascalTriangle
+{
+    class PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+            long height = long.Parse(Console.ReadLine());
+         
+            long[][] triangle = new long[height][];
+            for (int currentHeight = 0; currentHeight < height; currentHeight++)
+            {
+                triangle[currentHeight] = new long[currentHeight + 1];
+                triangle[currentHeight][0] = 1;
+                triangle[currentHeight][currentHeight] = 1;
+                if (currentHeight >= 2)
+                {
+                    for (int widhtCounter = 1; widhtCounter < currentHeight; widhtCounter++)
+                    {
+                        triangle[currentHeight][widhtCounter] = triangle[currentHeight - 1][widhtCounter - 1] +
+                        triangle[currentHeight - 1][widhtCounter];    
+                            
+                    }
+                }
+
+            }
+            for (int row = 0; row < triangle.Length; row++)
+            {
+                for (int col = 0; col < triangle[row].Length; col++)
+                {
+                    Console.Write(triangle[row][col] +" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
